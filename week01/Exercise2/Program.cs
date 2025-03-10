d0cbd6f97e2605e738b78c22d51d65f36833eb2c
ngahirinuki <hir23003@byui.edu>
2025-03-11 11:30:29 +1300

    Stretch challenge 1

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,61 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string number = Console.ReadLine();
+        int grade = int.Parse(number);
+        string letter = "";
+        string sign = "";
+        
+
+        if (grade >= 90)
+            {
+            letter = "A";
+            }
+            else if (grade >= 80)
+            {
+            letter = "B";
+            }
+            else if (grade >= 70)
+            {
+            letter = "C";
+            }
+            else if (grade >= 60)
+            {
+            letter = "D";
+            }
+            else
+            {
+            letter = "E";
+            }
+        
+        float divide = grade/10f;
+        int signage = grade/10;
+        float deci = (divide - signage) * 10;
+
+        if (deci >= 7)
+            {
+            sign = "+";
+            }
+            else if (deci <= 3)
+            {
+            sign = "-";
+            }
+            else
+            {
+            sign = " ";
+            }
+
+        Console.Write($"{letter}{sign}   {deci:F0} ");
+        
+        if (grade > 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed!");
+        }
     }
+        
 }
