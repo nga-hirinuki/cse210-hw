51f1de33a8ce3e2ecf302089fa52667b29c4a45e
ngahirinuki <hir23003@byui.edu>
2025-03-13 00:12:14 +1300

    Exe 4 added notes

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -10,33 +10,32 @@ class Program
 
         List<int> numbers = new List<int>();// Declare and initialize the list
         Console.WriteLine(" a list of numbers, type 0 when finished "); // ask question
-        int number;
-        float total;
-        float average;
+        int number; // Declare number variable
+        float total;// Declare total variable
+        float average;// Declare average variable
         do
         {
             Console.Write("Enter number? "); // body: guess number
-            number = int.Parse(Console.ReadLine());
+            number = int.Parse(Console.ReadLine());// convert string to int
 
-            if (number != 0)
+            if (number != 0) // declare conditions
             {
-                numbers.Add(number);
+                numbers.Add(number); // entered number is added to list
             }
 
         } while (number != 0);//guess until right
 
-        total = numbers.Sum();
-        average = total / numbers.Count;
+        total = numbers.Sum(); // declare sum of numbers in the list
+        average = total / numbers.Count;// use total divide / number in list
 
-        int largest = numbers[0];
-        foreach (int num in numbers)
+        int largest = numbers[0]; // declare variable
+        foreach (int num in numbers) // look into list 
         {
-            if (num > largest)
+            if (num > largest) // show condition to find largest
             {
-                largest = num;
+                largest = num; // show large number
             }
         }
-
         Console.WriteLine($"Total {total}");
         Console.WriteLine($"Count {numbers.Count}");
         Console.WriteLine($"Average {average:F2}");
