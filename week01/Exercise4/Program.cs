602f5cb0bc9613c831fdc6484e0f40f122c71f92
ngahirinuki <hir23003@byui.edu>
2025-03-12 23:37:20 +1300

    Exe4 created list, Asked for total, Count, Average and Largest

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,45 @@
 using System;
+using System.Collections.Generic;
+using System.Transactions;
+using System.Xml.XPath;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        List<int> numbers = new List<int>();// Declare and initialize the list
+        Console.WriteLine(" a list of numbers, type 0 when finished "); // ask question
+        int number;
+        float total;
+        float average;
+        do
+        {
+            Console.Write("Enter number? "); // body: guess number
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+            {
+                numbers.Add(number);
+            }
+
+        } while (number != 0);//guess until right
+
+        total = numbers.Sum();
+        average = total / numbers.Count;
+
+        int largest = numbers[0];
+        foreach (int num in numbers)
+        {
+            if (num > largest)
+            {
+                largest = num;
+            }
+        }
+
+        Console.WriteLine($"Total {total}");
+        Console.WriteLine($"Count {numbers.Count}");
+        Console.WriteLine($"Average {average:F2}");
+        Console.WriteLine($"Largest {total}");
     }
 }
