91d6cbe5c8ec07efda07ebd445e1c08d18f99fa4
ngahirinuki <hir23003@byui.edu>
2025-04-17 14:10:01 +1200

    finish Learning Activity Week6

---
+++ week06/Shapes/Square.cs
@@ -0,0 +1,19 @@
+public class Square : Shape
+{
+    private double _side;
+
+    public double GetSide()
+    {
+        return _side;
+    }
+    public void SetSide(double side)
+    {
+        _side = side;
+    }
+       
+    public override double GetArea()
+    {
+      return _side * _side;
+    }
+}
+
